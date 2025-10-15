8873f5e44a03450037548d89b5734b439fc8fdd9
Ilko <ilko@ilkop.com>
2025-10-15 02:59:08 +0200

    put the pc into the view

--- src/main.js
+++ src/main.js
@@ -1,24 +1,77 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'
+import { LoadGLTFByPath } from './Helpers/ModelHelper.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//Renderer does the job of rendering the graphics
+let renderer = new THREE.WebGLRenderer({
 
-setupCounter(document.querySelector('#counter'))
+    //Defines the canvas component in the DOM that will be used
+    canvas: document.querySelector('#app'),
+    antialias: true,
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//set up the renderer with the default settings for threejs.org/editor - revision r153
+renderer.shadows = true;
+renderer.shadowType = 1;
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.toneMapping = 0;
+renderer.toneMappingExposure = 1
+renderer.useLegacyLights = false;
+renderer.toneMapping = THREE.NoToneMapping;
+renderer.setClearColor(0xffffff, 0);
+//make sure three/build/three.module.js is over r152 or this feature is not available. 
+renderer.outputColorSpace = THREE.SRGBColorSpace
+
+const scene = new THREE.Scene();
+
+let cameraList = [];
+
+let camera;
+
+// Load the GLTF model
+LoadGLTFByPath(scene)
+    .then(() => {
+        retrieveListOfCameras(scene);
+    })
+    .catch((error) => {
+        console.error('Error loading JSON scene:', error);
+    });
+
+//retrieve list of all cameras
+function retrieveListOfCameras(scene) {
+    // Get a list of all cameras in the scene
+    scene.traverse(function (object) {
+        if (object.isCamera) {
+            cameraList.push(object);
+        }
+    });
+
+    //Set the camera to the first value in the list of cameras
+    camera = cameraList[0];
+
+    updateCameraAspect(camera);
+
+    // Start the animation loop after the model and cameras are loaded
+    animate();
+}
+
+// Set the camera aspect ratio to match the browser window dimensions
+function updateCameraAspect(camera) {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+}
+
+//A method to be run each time a frame is generated
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+};
+
+
+
+
